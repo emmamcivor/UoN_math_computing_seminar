6072e6e33e90144fa2445b263fd03f9375f39284
Emma McIvor <emmamcivor23@gmail.com>
2019-03-20 15:27:37 +0000

    example 1 is working

--- test.m
+++ test.m
@@ -7,16 +7,8 @@ function test(a,b)
 c=a+b;
 d=a*b;
 
-% name a folder and file to save the output of the simulations
-fn_save=['test_save/test_GNU_parallel-a_',num2str(a),'-b_',num2str(b),'.mat'];
-
-% make the folder if it does not already exist
-if exist(fn_save)
-else
-mkdir(fn_save)
-end
-
 % save the ouputs to a file in test_save folder with parameters making up filename
+fn_save=['test_GNU_parallel-a_',num2str(a),'-b_',num2str(b),'.mat'];
 save(fn_save,'c','d')
 
 % print to standard out which can be caught by GNU parallel
