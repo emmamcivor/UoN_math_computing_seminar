307becc45d3f402d3f20cb3a7b740b56bcd99dc9
Emma McIvor <emmamcivor23@gmail.com>
2019-03-20 22:27:54 +0000

    removed example with multiple hosts as I haven't set up ssh keys so it won't work (TODO as a future example). started a summary of the process in a presentation

--- ex1_SINGLE_MACHINE/test.m
+++ ex1_SINGLE_MACHINE/test.m
@@ -7,13 +7,15 @@ function test(a,b)
 c=a+b;
 d=a*b;
 
-% save the ouputs to a file in test_save folder with parameters making up filename
+% save the outputs to a file in test_save folder with parameters making
+% up filename
 fn_save=['test_GNU_parallel-a_',num2str(a),'-b_',num2str(b),'.mat'];
 save(fn_save,'c','d')
 
 % print to standard out which can be caught by GNU parallel
 fprintf(1,"\n\n[DATA]%d,%d,%d,%d\n\n",a,b,c,d);
 
-% I found that I had to exit matlab explicitly but this might not be the case for other languages
+% I found that I had to exit matlab explicitly but this might not be 
+% the case for other languages
 exit; 
 end
